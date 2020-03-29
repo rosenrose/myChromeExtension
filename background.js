150361eb14a981b85eb2711f19a5ef7f93f45920
rosenrose <42435748+rosenrose@users.noreply.github.com>
2020-03-30 03:57:06 +0900

    tab to space

--- background.js
+++ background.js
@@ -1,19 +1,19 @@
 chrome.runtime.onInstalled.addListener(function() {
-	chrome.storage.sync.set({
-		color: '#000000',
-		banList: []
-	}, function() {
-		console.log('Black');
-	});
-	// chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-	// 	chrome.declarativeContent.onPageChanged.addRules([{
-	// 		conditions: [new chrome.declarativeContent.PageStateMatcher({
-	// 			pageUrl: {
-	// 				hostEquals: 'developer.chrome.com',
-	// 				urlContains: 'ruliweb.com'
-	// 			},
-	// 		})],
-	// 		actions: [new chrome.declarativeContent.ShowPageAction()]
-	// 	}]);
-	// });
+    chrome.storage.sync.set({
+        color: '#000000',
+        banList: []
+    }, function() {
+        console.log('Black');
+    });
+    // chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    // 	chrome.declarativeContent.onPageChanged.addRules([{
+    // 		conditions: [new chrome.declarativeContent.PageStateMatcher({
+    // 			pageUrl: {
+    // 				hostEquals: 'developer.chrome.com',
+    // 				urlContains: 'ruliweb.com'
+    // 			},
+    // 		})],
+    // 		actions: [new chrome.declarativeContent.ShowPageAction()]
+    // 	}]);
+    // });
 });
